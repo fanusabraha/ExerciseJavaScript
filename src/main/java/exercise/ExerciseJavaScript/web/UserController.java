8fd7b3778f2440bdd53987e512ffc77cde8db775
fanus abraha <fanus.abraha@yahoo.com>
2024-05-07 12:14:05 -0500

    Initial commit

---
+++ src/main/java/exercise/ExerciseJavaScript/web/UserController.java
@@ -0,0 +1,12 @@
+package exercise.ExerciseJavaScript.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/login")
+    public String userLogin(){
+        return "login";
+    }
+}
