b42cd6ed11336794b2bc8d2059cb43787367d08d
fanus abraha <fanus.abraha@yahoo.com>
2024-05-07 12:17:03 -0500

    Update UserController.java

--- src/main/java/exercise/ExerciseJavaScript/web/UserController.java
+++ src/main/java/exercise/ExerciseJavaScript/web/UserController.java
@@ -1,12 +1,13 @@
 package exercise.ExerciseJavaScript.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/login")
-    public String userLogin(){
+    public String userLogin(ModelMap map){
         return "login";
     }
 }
