5c0c957f948662b1b62cecafe2fb542ff99462f3
fanus abraha <fanus.abraha@yahoo.com>
2024-11-22 16:53:17 -0600

    Merge remote-tracking branch 'origin/main'

--- src/main/java/exercise/ExerciseJavaScript/domain/User.java
+++ src/main/java/exercise/ExerciseJavaScript/domain/User.java
@@ -1,4 +1,18 @@
 package exercise.ExerciseJavaScript.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    private int Id;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
 }
--- src/main/java/exercise/ExerciseJavaScript/service/UserService.java
+++ src/main/java/exercise/ExerciseJavaScript/service/UserService.java
@@ -1,7 +1,11 @@
 package exercise.ExerciseJavaScript.service;
 
+import exercise.ExerciseJavaScript.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
 }
