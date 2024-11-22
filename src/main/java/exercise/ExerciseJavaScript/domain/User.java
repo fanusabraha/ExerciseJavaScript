aa450d82d02010e3fa678c810906fdb7b71a8314
fanus abraha <fanus.abraha@yahoo.com>
2024-11-22 16:57:12 -0600

    Merge remote-tracking branch 'origin/main'

--- src/main/java/exercise/ExerciseJavaScript/domain/User.java
+++ src/main/java/exercise/ExerciseJavaScript/domain/User.java
@@ -1,11 +1,14 @@
 package exercise.ExerciseJavaScript.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
-    @Id
+    @Id()
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int Id;
 
     public int getId() {
